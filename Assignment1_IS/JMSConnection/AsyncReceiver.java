9b87e098467732a9d87b3f5fe1b99ae44095adc1
Afonso Morais <Afonso Morais>
2016-10-06 18:19:10 +0100

    com ficheiros do jms mas com erro no .createContext, nao sei pq

---
+++ Assignment1_IS/JMSConnection/AsyncReceiver.java
@@ -0,0 +1,52 @@
+import java.io.IOException;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSConsumer;
+import javax.jms.JMSContext;
+import javax.jms.JMSException;
+import javax.jms.JMSRuntimeException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+
+public class AsyncReceiver implements MessageListener {
+    private ConnectionFactory cf;
+    private Destination d;
+
+    public AsyncReceiver() throws NamingException {
+        this.cf = InitialContext.doLookup("jms/RemoteConnectionFactory");
+        this.d = InitialContext.doLookup("jms/queue/PlayQueue");
+    }
+
+    @Override
+    public void onMessage(Message msg) {
+        TextMessage tmsg = (TextMessage) msg;
+        try {
+            System.out.println("Got message: " + tmsg.getText());
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void launch_and_wait() {
+        try (JMSContext jcontext = cf.createContext("test", "test");) {
+            JMSConsumer consumer = jcontext.createConsumer(d);
+            consumer.setMessageListener(this);
+            System.out.println("Press enter to finish...");
+            System.in.read();
+        } catch (JMSRuntimeException | IOException re) {
+            re.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws NamingException {
+        AsyncReceiver r = new AsyncReceiver();
+        r.launch_and_wait();
+    }
+
+
+
+}
