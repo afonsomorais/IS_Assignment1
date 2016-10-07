aa27969295ea44851c1a187b35cef37bfbd6c589
Afonso Morais <Afonso Morais>
2016-10-07 15:17:41 +0100

    crawler em construção, ainda erro no naming

--- Assignment1_IS/Crawler/Crawler.java
+++ Assignment1_IS/Crawler/Crawler.java
@@ -1,3 +1,4 @@
+import com.sun.org.apache.xpath.internal.SourceTree;
 import org.jsoup.Jsoup;
 import org.jsoup.helper.Validate;
 import org.jsoup.nodes.Document;
@@ -8,22 +9,30 @@ import java.io.IOException;
 
 
 public class Crawler {
-    
+
     public static void main(String[] args) throws IOException {
-        String url = "http://news.ycombinator.com/";
-        print("Fetching %s...", url);
+        String url = "https://www.rio2016.com/en/medal-count-country";
+        System.out.println("Fetching "+url+"...");
 
         Document doc = Jsoup.connect(url).get();
-        Elements links = doc.select("a[href]");
+        /*Elements links = doc.select("a[href]");
         Elements media = doc.select("[src]");
-        Elements imports = doc.select("link[href]");
+        Elements imports = doc.select("link[href]");*/
+        Element tableMedals = doc.select("table").get(2);
+        Elements rows = tableMedals.select("tr");
+        
+        System.out.println("La dentro: "+rows.get(124).toString());
+        //System.out.println("ola: "+ tableMedals.text().toString());
+        //System.out.println("Media: "+tableMedals.size());
 
-        print("\nMedia: (%d)", media.size());
+
+
+        /*print("\nMedia: (%d)", media.size());
         for (Element src : media) {
             if (src.tagName().equals("img"))
                 print(" * %s: <%s> %sx%s (%s)",
-                        src.tagName(), src.attr("abs:src"), src.attr("width"), src.attr("height"),
-                        trim(src.attr("alt"), 20));
+                    src.tagName(), src.attr("abs:src"), src.attr("width"), src.attr("height"),
+                    trim(src.attr("alt"), 20));
             else
                 print(" * %s: <%s>", src.tagName(), src.attr("abs:src"));
         }
@@ -37,16 +46,13 @@ public class Crawler {
         for (Element link : links) {
             print(" * a: <%s>  (%s)", link.attr("abs:href"), trim(link.text(), 35));
         }
-    }
+    }*/
 
-    private static void print(String msg, Object... args) {
-        System.out.println(String.format(msg, args));
-    }
-
-    private static String trim(String s, int width) {
+    /*private static String trim(String s, int width) {
         if (s.length() > width)
             return s.substring(0, width-1) + ".";
         else
             return s;
+    */
     }
 }
